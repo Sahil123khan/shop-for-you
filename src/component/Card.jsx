c451a09c24dfe31903e8d0f8e8aaa63430eac5c7
Sahil123khan <mrffking04@gmail.com>
2025-06-24 20:24:51 +0530

    Add Card component and integrate it into Home page for displaying product data

---
+++ src/component/Card.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+const Card = ({getdata}) => {
+    const [expande,setexpande] = useState(false)
+    const [scrollon,setscrolloff]=useState(false)
+  return (
+    <div className='w-64 h-80  m-4 rounded shadow-sm flex flex-col items-center justify-between p-4'>
+      <div className='w-full h-40 flex items-center justify-center  rounded'>
+        <img src={getdata.image} alt="imageloading" className='max-h-36 object-contain'/>
+      </div>
+      <div className={`w-full mt-2 ${scrollon ? `overflow-scroll` : ''}`}>
+        <h1 className={`${expande ? '' : `line-clamp-1`}   text-lg font-semibold`}>{getdata.title}</h1>
+        <p className={`${expande ? '' : `line-clamp-2`}`}>{getdata.description}</p>
+        <button
+          onClick={() => {
+            setexpande(e => {
+              if (e) {
+                setscrolloff(false);
+              } else {
+                setscrolloff(true);
+              }
+              return !e;
+            });
+          }}
+          className='text-blue-300 font-semibold '
+        >
+          {`${expande ? 'close' : 'seemore...'}`}
+        </button>
+        <h5>{`Price = ₹${getdata.price}`}</h5>
+      </div>
+    </div>
+  )
+}
+
+export default Card
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import  axios from 'axios'
+import Card from '../component/Card';
 const Home =()=> {
 
   const [apidata , setapidata] = useState([]);
@@ -9,25 +10,35 @@ const API = "https://fakestoreapi.com/products";
 const getApiData = async () => {
  try {
   const res = await axios.get(API)
-  setapidata(res);
-  console.log('apidata',apidata);
+  console.log('apidata',res.data);
+
+  setapidata(res.data);
   
  } catch (error) {
   console.log("error",error);
   
  }
+
 };
 
 useEffect(()=>{
  getApiData();
 },[]);
 
+
+
   return (
-      <div>
-        <h1>Home</h1>
-      </div>
-    )
-  }
+    <div className="flex flex-wrap justify-center items-start">
+      {
+        apidata.map((currentelm)=>{
+          return(
+            <Card key={currentelm.id} getdata={currentelm}/>
+          );
+        })
+      }
+    </div>
+  )
+}
 
 
 export default Home
